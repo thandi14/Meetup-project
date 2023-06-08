5a6ffaa97788f8d53f1c364a4c2b56c6120d6af8
thandi mpofu <thandimpofu2003@gmail.com>
2023-06-08 16:35:11 -0500

    another update

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 const {
-  Model
+  Model, Validator
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -44,6 +44,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+    defaultScope: {
+      attributes: {
+        exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+      }
+    }
   });
   return User;
 };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+
+
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('Hello World!');
@@ -18,5 +25,31 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
+router.get('/set-token-cookie', async (_req, res) => {
+    const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+    setTokenCookie(res, user);
+    return res.json({ user: user });
+});
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 module.exports = router;
