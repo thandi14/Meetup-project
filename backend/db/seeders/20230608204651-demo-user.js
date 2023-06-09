43061b0769bba697e9d5bd3715e94a6c50cc3ef7
thandi mpofu <thandimpofu2003@gmail.com>
2023-06-09 16:51:56 -0500

    deploy8

--- backend/db/seeders/20230608204651-demo-user.js
+++ backend/db/seeders/20230608204651-demo-user.js
@@ -21,16 +21,19 @@ module.exports = {
      options.tableName = 'Users';
      return queryInterface.bulkInsert(options, [
        {
+         lastName: "demo",
          email: 'demo@user.io',
          username: 'Demo-lition',
          hashedPassword: bcrypt.hashSync('password')
        },
        {
+         lastName: "user1",
          email: 'user1@user.io',
          username: 'FakeUser1',
          hashedPassword: bcrypt.hashSync('password2')
        },
        {
+        lastName: "user2",
          email: 'user2@user.io',
          username: 'FakeUser2',
          hashedPassword: bcrypt.hashSync('password3')
