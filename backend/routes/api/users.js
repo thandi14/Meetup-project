af7d50c305bde825d93ae7610d511b98a581ac9d
thandi mpofu <thandimpofu2003@gmail.com>
2023-06-09 16:22:30 -0500

    deploy6

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,26 +7,6 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
-// router.post(
-//     '/',
-//     async (req, res) => {
-//         const { email, password, username } = req.body;
-//         const hashedPassword = bcrypt.hashSync(password);
-//         const user = await User.create({ email, username, hashedPassword });
-
-//         const safeUser = {
-//             id: user.id,
-//             email: user.email,
-//             username: user.username,
-//         };
-
-//         await setTokenCookie(res, safeUser);
-
-//         return res.json({
-//             user: safeUser
-//         });
-//     }
-//     );
 
     const { check } = require('express-validator');
     const { handleValidationErrors } = require('../../utils/validation');
